fc58aa40c3e848fb47ee59ad3be10151f381519f
nicsylvia <nicsylvia15f@gmail.com>
2023-03-10 13:31:56 -0800

    saved all

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,28 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 
 import cors from "cors";
 
 import morgan from "morgan"
+import { ErrorHandler } from "./Middlewares/ErrorHandler";
+import { AppError, HTTPCODES } from "./Utils/AppError";
 
 const AppConfig = (app: Application) =>{
     app.use(express.json())
     app.use(cors())
     app.use(morgan("dev"))
 
+    // Routes that does not exist
+    app.all("*", (req: Request, res: Response, next: NextFunction) =>{
+        next(
+            new AppError({
+                message: `This route, ${req.originalUrl} is not found`,
+                httpcode: HTTPCODES.NOT_FOUND
+            })
+        )
+    })
+
     // Error Handler
+    app.use(ErrorHandler)
 }
 
 export default AppConfig;
