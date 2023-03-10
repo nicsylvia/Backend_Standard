4fb22e09bc09e0663380f868d36c92b99a08db0c
nicsylvia <nicsylvia15f@gmail.com>
2023-03-10 13:56:48 -0800

    saved all

---
+++ src/API/index.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+import homeroutes from "../Routes/homeroutes";
+
+const router = Router()
+
+router.route("/")
--- src/Routes/homeroutes.ts
+++ src/Routes/homeroutes.ts
@@ -4,8 +4,12 @@ const router = Router();
 
 router.get("/", (req: Request, res: Response, next: NextFunction) =>{
     try {
-        
+        res.status(200).json({
+            message: "Welcome to Set06 Standard API"
+        })
     } catch (error) {
-        
+        next(error)
     }
 })
+
+export default router
