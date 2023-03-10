2d0f20f5eeae11fdfe39a57726f41c026767c615
nicsylvia <nicsylvia15f@gmail.com>
2023-03-10 11:13:31 -0800

    saved all

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import express, { Application } from "express";
 
-import cors from "cors";
-
 const port: number = 3500;
 
 const app: Application = express()
 
 app.listen(port, () =>{
     console.log("");
-    console.log("Server is running")
+    console.log("Server is up and running")
 })
