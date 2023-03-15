573cdc4ecaa0e914bbbb7777c35a13f3e400b63e
nicsylvia <nicsylvia15f@gmail.com>
2023-03-15 12:16:24 -0700

    saved business logic

--- src/Interfaces/UserInterface.ts
+++ src/Interfaces/UserInterface.ts
@@ -4,5 +4,6 @@ export interface IUSER extends Document{
     name: string;
     email: string;
     password: string;
+    confirmPassword: string;
     role: string
 }
--- src/Models/UserModels.ts
+++ src/Models/UserModels.ts
@@ -2,6 +2,8 @@ import { Schema, model } from "mongoose";
 
 import { IUSER } from "../Interfaces/UserInterface";
 
+import isEmail from "validator/lib/isEmail"
+
 const UserSchema: Schema<IUSER> = new Schema({
     name: {
         type: String,
@@ -12,14 +14,29 @@ const UserSchema: Schema<IUSER> = new Schema({
         required: [true, "Please enter your email"],
         unique: true,
         lowercase: true,
-        trim: true
+        trim: true,
+        validate: [isEmail, "Please enter a valid email"],
     },
     password: {
         type: String,
         required: [true, "Please enter your Password"]
     },
+    confirmPassword: {
+        type: String,
+        required: [true, "Please confirm your password"]
+    },
     role: {
         type: String,
-        required: [true, "Please enter your name"]
+        required: [true, "Please enter your role"],
+        message: "You must either be a User, Manager or Admin",
+        enum: ["User", "Admin", "Manager"],
+        default: "User"
     },
+},
+{
+    timestamps: true
 })
+
+const UserModel = model<IUSER>("User", UserSchema);
+
+export default UserModel;
