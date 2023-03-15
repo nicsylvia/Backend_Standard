86b1bd8e435b77c32709a930fee3cf9ad05f6504
nicsylvia <nicsylvia15f@gmail.com>
2023-03-15 11:58:08 -0700

    saved validator

--- src/Models/UserModels.ts
+++ src/Models/UserModels.ts
@@ -1,3 +1,25 @@
 import { Schema, model } from "mongoose";
 
 import { IUSER } from "../Interfaces/UserInterface";
+
+const UserSchema: Schema<IUSER> = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your Password"]
+    },
+    role: {
+        type: String,
+        required: [true, "Please enter your name"]
+    },
+})
