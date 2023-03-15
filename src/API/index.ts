4a686d18a65b90bc2be616329d00ff0f19489411
nicsylvia <nicsylvia15f@gmail.com>
2023-03-15 15:15:19 -0700

    saved all

--- src/API/index.ts
+++ src/API/index.ts
@@ -2,8 +2,12 @@ import { Router } from "express";
 
 import homeroutes from "../Routes/homeroutes";
 
+import Authroutes from "../Routes/AuthRoutes"
+
 const router = Router()
 
-router.get("/", homeroutes)
+// router.get("/", homeroutes)
+
+router.use("/", Authroutes)
 
 export default router
